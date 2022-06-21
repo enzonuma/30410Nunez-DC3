4f52ace3499e17f35990ed5146620671605d0f91
enzonuma <enzo.nmateluna@gmail.com>
2022-06-20 22:09:15 -0400

    Arreglo boton mas función de agregar a html

--- js/prodDealer.js
+++ js/prodDealer.js
@@ -32,22 +32,38 @@ function addProduct() {
         constructor(prodCate, prodName, prodPrice) {
             this.prodCate = prodCate
             this.prodName = prodName
-            this.prodPrice = prodPrice
+            this.prodPrice = parseInt(prodPrice)
         }
     }
 
-    prodCate = prompt("Do you want to add: Shoe | Clothes | Accesories ?");
-    prodName = prompt("Enter the Model/Name:");
-    prodPrice = prompt("Enter the price:");
-    debugger
-    prodList.push(new Product(prodCate, prodName, prodPrice));
+    prodCate = prompt("Do you want to add: Shoe | Clothes | Accesories ?")
+    prodName = prompt("Enter the Model/Name:")
+    prodPrice = prompt("Enter the price:")
+    prodList.push(new Product(prodCate, prodName, prodPrice))
     congratstext = prompt("Your products were added succesfully! Do you want to add another ones? (YY / NN to close)")
 }
 
-const btnAddProd = document.querySelector("#addProd")
+function showProd() {
+    const prodCards = document.querySelector("#prodCatalog");
+    prodCards.innerHTML = ""
+
+    prodList.forEach( prod => {
+        const prodDiv = document.createElement("div")
+        prodDiv.classList.add("col-4", "prodCards")
+        prodDiv.innerHTML += `<img src="">
+        <p class="pcCategory">${prod.prodCate}</p>
+        <h6 class="pcTitle">${prod.prodName}</h6>
+        <p class="pcPrice">$${prod.prodPrice}</p>`
+
+        prodCards.appendChild(prodDiv)
+    });
+}
+
+let btnAddProd = document.querySelector("#addProd")
 btnAddProd.addEventListener("click", () => {
     addProduct()
     congrats()
+    showProd()
 })
 
 // export { prodList };
